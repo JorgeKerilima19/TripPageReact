b625923679688c5a693f0a7adf01b327deb004e8
JorgeMioska <quispechilojorge@gmail.com>
2023-02-01 05:21:38 -0500

    navbar map display

---
+++ src/js/navbarItems.js
@@ -0,0 +1,7 @@
+export const NavItems = [
+  { title: "Home", url: "/", className: "", icon: "" },
+  { title: "About", url: "/", className: "", icon: "" },
+  { title: "Service", url: "/", className: "", icon: "" },
+  { title: "Contact", url: "/", className: "", icon: "" },
+  { title: "Register", url: "/", className: "", icon: "" },
+];
--- src/jsx/Navbar.jsx
+++ src/jsx/Navbar.jsx
@@ -1,9 +1,19 @@
 import "../styles/navbarStyle.css";
+import { NavItems } from "../js/navbarItems";
 
 export default function Navbar() {
   return (
     <header>
-      <div>Hello Wold</div>
+      <ul>{NavItems.map((item, index)=>{
+        return(
+          <li key={index}>
+            <a href="/">
+              <svg className={item.className}></svg>
+              {item.title}
+            </a>
+          </li>
+        )
+      })}</ul>
     </header>
   );
 }
