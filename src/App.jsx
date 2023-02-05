7899eb16926521cfc38ab9bf7f33747414a6d153
JorgeMioska <quispechilojorge@gmail.com>
2023-02-05 05:46:40 -0500

    general styles + navbar flex

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Navbar from "./jsx/navbar";
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
     <Navbar/>
     </>
   );
--- src/jsx/Navbar.jsx
+++ src/jsx/Navbar.jsx
@@ -1,19 +1,25 @@
+import "../index.css";
 import "../styles/navbarStyle.css";
 import { NavItems } from "../js/navbarItems";
 
 export default function Navbar() {
   return (
-    <header>
-      <ul>{NavItems.map((item, index)=>{
-        return(
-          <li key={index}>
-            <a href="/">
-              <svg className={item.className}></svg>
-              {item.title}
-            </a>
-          </li>
-        )
-      })}</ul>
+    <header className="header">
+      <nav className="nav">
+        <svg className="svg-holder-image"></svg>
+        <ul className="navbar">
+          {NavItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a href="/">
+                  <svg className={item.className}></svg>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
     </header>
   );
 }
