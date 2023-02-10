12eacd3f0f24bf51281894b63a2cf01d9720ef00
JorgeMioska <quispechilojorge@gmail.com>
2023-02-09 19:49:56 -0500

    image src

--- src/js/navbarItems.js
+++ src/js/navbarItems.js
@@ -1,22 +1,42 @@
 export const NavItems = [
-  { title: "Home", url: "/", className: "svg-holder-image", icon: "" },
-  { title: "About", url: "/about", className: "svg-holder-image", icon: "" },
+  {
+    title: "Home",
+    url: "/",
+    className: "svg-holder-image",
+    image:
+      "https://images.unsplash.com/photo-1530789253388-582c481c54b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    icon: "",
+  },
+  {
+    title: "About",
+    url: "/about",
+    className: "svg-holder-image",
+    image:
+      "https://images.unsplash.com/photo-1503220317375-aaad61436b1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+    icon: "",
+  },
   {
     title: "Service",
     url: "/service",
     className: "svg-holder-image",
+    image:
+      "https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=821&q=80",
     icon: "",
   },
   {
     title: "Contact",
     url: "/contact",
     className: "svg-holder-image",
+    image:
+      "https://images.unsplash.com/photo-1539635278303-d4002c07eae3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
     icon: "",
   },
   {
     title: "Register",
     url: "/register",
     className: "svg-holder-image",
+    image:
+      "https://images.unsplash.com/photo-1473625247510-8ceb1760943f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=811&q=80",
     icon: "",
   },
 ];
