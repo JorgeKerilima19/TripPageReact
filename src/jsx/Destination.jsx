f9d9468e1bc350edd8ee65cee90bf2f0e8fb9142
JorgeMioska <quispechilojorge@gmail.com>
2023-02-10 23:02:32 -0500

    popular destination

---
+++ src/jsx/Destination.jsx
@@ -0,0 +1,18 @@
+export default function Destinations() {
+  return (
+    <>
+      <img
+        src="https://images.unsplash.com/photo-1415804941191-bc0c3bbac10d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+        alt=""
+      />
+      <h3>HERE GOES THE TITLE</h3>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. In similique
+        nisi dicta dignissimos debitis assumenda exercitationem sapiente
+        asperiores provident doloribus corrupti nostrum corporis fuga culpa ad
+        officiis delectus, at dolores.
+      </p>
+      <button>Book</button>
+    </>
+  );
+}
--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,5 +1,12 @@
 import HeroImage from "../jsx/HeroImage";
+import Destinations from "../jsx/Destination";
 
 export default function Home() {
-  return <HeroImage src={"Home"}></HeroImage>;
+  return (
+    <>
+      <HeroImage src={"Home"}></HeroImage>
+      <h2>{"Popular Destinations"}</h2>
+      <Destinations></Destinations>
+    </>
+  );
 }
