684f38657afb7f6a85df1bcf62950084f28ca028
JorgeMioska <quispechilojorge@gmail.com>
2023-02-11 23:55:53 -0500

    responsive changes

--- src/data/destination.js
+++ src/data/destination.js
@@ -21,6 +21,14 @@ export const Destinations = [
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit. In similique nisi dicta dignissimos debitis assumenda exercitationem sapiente asperiores provident doloribus corrupti nostrum corporis fuga culpa ad officiis delectus, at dolores.",
     src1: "https://images.unsplash.com/photo-1544989164-22f292ae11b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
-    src2: "https://images.unsplash.com/photo-1518639192441-8fce0a366e2e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80",
+    src2: "https://images.unsplash.com/photo-1655642272518-05aaf7b8726e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=433&q=80",
+  },
+  {
+    title: "Argentina",
+    popular: "Buenos Aires",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. In similique nisi dicta dignissimos debitis assumenda exercitationem sapiente asperiores provident doloribus corrupti nostrum corporis fuga culpa ad officiis delectus, at dolores.",
+    src1: "https://images.unsplash.com/photo-1589909202802-8f4aadce1849?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80",
+    src2: "https://images.unsplash.com/photo-1495317823589-e67efe1524b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
   },
 ];
--- src/jsx/Destination.jsx
+++ src/jsx/Destination.jsx
@@ -6,12 +6,14 @@ export default function Destinations({ item }) {
           <img src={item.src1} alt="Image1" />
           <img src={item.src2} alt="Image2" />
         </div>
-        <h3 className="card-title">
-          {item.title},{" "}
-          <small className="card-title_small">{item.popular}</small>
-        </h3>
-        <p className="card-description">{item.description}</p>
-        <button className="button-medium">Book</button>
+        <div className="destination-card_description">
+          <h3 className="card-title">
+            {item.title},{" "}
+            <small className="card-title_small">{item.popular}</small>
+          </h3>
+          <p className="card-description">{item.description}</p>
+          <button className="button-medium">Book</button>
+        </div>
       </article>
     </>
   );
