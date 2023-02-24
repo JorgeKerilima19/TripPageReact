6dd9d3e4a827459330f4a175aa2aab53a0fa3bdd
JorgeMioska <quispechilojorge@gmail.com>
2023-02-24 00:58:56 -0500

    contact section ready

--- src/jsx/Form.jsx
+++ src/jsx/Form.jsx
@@ -1,8 +1,7 @@
 function Form() {
   return (
     <section className="section__contact">
-      <div className="contact__img-container">
-      </div>
+      <div className="contact__img-container"></div>
       <div className="contact__form-container">
         <h2>Keep In Touch</h2>
         <p>
@@ -12,17 +11,29 @@ function Form() {
         <form action="" className="form-container__form">
           <div className="form__input-container">
             <label htmlFor="">Name: </label>
-            <input className="input-container__input" type="text" placeholder="Name" />
+            <input
+              className="input-container__input"
+              type="text"
+              placeholder="Name"
+            />
           </div>
           <div className="form__input-container">
             <label htmlFor="">Last-name: </label>
-            <input className="input-container__input" type="text" placeholder="Name" />
+            <input
+              className="input-container__input"
+              type="text"
+              placeholder="Last-name"
+            />
           </div>
           <div className="form__input-container">
             <label htmlFor="">Email: </label>
-            <input className="input-container__input" type="text" placeholder="Name" />
+            <input
+              className="input-container__input"
+              type="text"
+              placeholder="e-mail"
+            />
           </div>
-          <div className="">
+          <div className="input-container__radio-container">
             <div>
               <input type="radio" name="type" />
               <label>Suggestions</label>
@@ -36,15 +47,15 @@ function Form() {
               <label>Consultation</label>
             </div>
           </div>
-          <div className="form__input-container">
+          <div className="form__input-container-textarea">
             <label htmlFor="">Your message</label>
             <textarea></textarea>
           </div>
           <div className="">
             <input type="checkbox" />
-            <label>Accept Terms and conditions</label>
+            <label> Accept Terms and conditions</label>
           </div>
-          <button type="submit">Send Message</button>
+          <button className="form__button" type="submit">Send Message</button>
         </form>
       </div>
     </section>
