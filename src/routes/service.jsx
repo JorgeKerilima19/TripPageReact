150d34931955be93a432a4da0bdfb88a0d7fb8aa
JorgeMioska <quispechilojorge@gmail.com>
2023-02-28 06:33:03 -0500

    service to work

--- src/routes/service.jsx
+++ src/routes/service.jsx
@@ -1,10 +1,21 @@
 import HeroImage from "../jsx/HeroImage";
 import Footer from "../jsx/Footer";
+import Destinations from "../jsx/Destination";
+
+import { Destinations as data } from "../data/destination";
 
 export default function Services() {
   return (
     <>
       <HeroImage src={"Service"} />
+      <h2 className="section-title">Popular Destinations</h2>
+      <div className="first-container">
+        <section className="card-container">
+          {data.map((item, index) => {
+            return <Destinations key={index} item={item} />;
+          })}
+        </section>
+      </div>
       <Footer />
     </>
   );
