bcef2a19504bfabdb05b2278f97c47d5b895a155
JorgeMioska <quispechilojorge@gmail.com>
2023-07-24 17:26:01 -0500

    info + card price

--- src/jsx/TourCard.jsx
+++ src/jsx/TourCard.jsx
@@ -1,8 +1,15 @@
 const TourCard = ({ tour }) => {
   return (
-    <div className="card__container-md flex flex__column flex__gap-sm flex__item-center">
-      <img className="destination-card__img width__90" src={tour.banner} alt="" />
-      {tour.name}
+    <div className="card__container-md flex flex__column flex__gap-sm flex__item-center ps__relative">
+      <img
+        className="destination-card__img width__90"
+        src={tour.banner}
+        alt=""
+      />
+      <div className="flex flex__sp-btw width__90">
+        <h4 className="card__title">{tour.name}</h4>
+        <span className="card__price">$/{tour.pricePerPerson}</span>
+      </div>
     </div>
   );
 };
