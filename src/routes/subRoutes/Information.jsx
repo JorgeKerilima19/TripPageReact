1baefc33d5d23a867eafe0613cc1089c51dade69
JorgeMioska <quispechilojorge@gmail.com>
2023-07-23 21:09:53 -0500

    .

--- src/routes/subRoutes/Information.jsx
+++ src/routes/subRoutes/Information.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import { useContext } from "react";
+import { TourContext } from "../../context/tourContext";
 
 export const Information = () => {
-  return <div>Information</div>;
+  const { destination } = useContext(TourContext);
+
+  return (
+    <>
+      <div className="section__container flex__gap-sm width__60">
+        <h3 className="section__title">About {destination.name}</h3>
+        <p className="section__content tour__description  ">
+          {destination.about}
+        </p>
+        <div>About our tours to be</div>
+        <div>Popular tours</div>
+        <div>About our guides</div>
+        <div>blog+newsLetter</div>
+        <div>reviews</div>
+      </div>
+    </>
+  );
 };
--- src/routes/subRoutes/Places.jsx
+++ src/routes/subRoutes/Places.jsx
@@ -6,10 +6,12 @@ import TourCard from "../../jsx/TourCard";
 export const Places = () => {
   const { destination } = useContext(TourContext);
   return (
-    <section className="container__cards flex__sp-center">
-      {destination?.places?.map((tour) => (
-        <TourCard key={tour.placeId} tour={tour} />
-      ))}
-    </section>
+    <>
+      <section className="container__cards flex__sp-center">
+        {destination?.places?.map((tour) => (
+          <TourCard key={tour.placeId} tour={tour} />
+        ))}
+      </section>
+    </>
   );
 };
