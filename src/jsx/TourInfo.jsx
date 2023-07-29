44d1f0e28a0019c2311d7885c8f703f6e9570bad
JorgeMioska <quispechilojorge@gmail.com>
2023-07-29 00:45:41 -0500

    InfoTour completed

--- src/jsx/TourInfo.jsx
+++ src/jsx/TourInfo.jsx
@@ -7,10 +7,10 @@ const TourInfo = () => {
   return (
     <section className="flex flex__column flex__item-center flex__gap-bg width__80">
       <div className="flex flex__column flex__gap-md">
-        <h2>
+        <h2 className="section__title color__primary">
           {tour.name}
-          <span> {tour.pricePerPerson}</span>
-          <span>/per Person</span>
+          <span className="color__tertiary"> ${tour.pricePerPerson}</span>
+          <span className="tour__price color__tertiary">/per Person</span>
         </h2>
         <p className="tour-info__body">{tour.about}</p>
       </div>
@@ -20,13 +20,29 @@ const TourInfo = () => {
             <li key={index} className="tour-info__item">
               <h3>{key}</h3>
               {typeof value === "object" ? (
-                <span>
-                  {Object.entries(value).map(([subKey, subValue]) => (
-                    <p key={subKey}>
-                      {subKey}: {String(subValue)}
-                    </p>
-                  ))}
-                </span>
+                <div className="flex flex__gap-md">
+                  <div className="flex flex__column flex__gap-sm width__90">
+                    <h4>Included</h4>
+                    {Object.entries(value).map(([subKey, subValue]) => {
+                      if (subValue) {
+                        return (
+                          <span key={subKey}>
+                            {subKey}
+                            <span style={{ color: "green" }}>✔</span>
+                          </span>
+                        );
+                      }
+                    })}
+                  </div>
+                  <div className="flex flex__column flex__gap-sm width__90">
+                    <h4>Not Included </h4>
+                    {Object.entries(value).map(([subKey, subValue]) => {
+                      if (!subValue) {
+                        return <span key={subKey}>{subKey} ❌</span>;
+                      }
+                    })}
+                  </div>
+                </div>
               ) : (
                 <div>
                   <span>{value}</span>
@@ -36,7 +52,12 @@ const TourInfo = () => {
           );
         })}
       </ul>
-      <div>Photos</div>
+      <h4 className="section__subtitle">Photos</h4>
+      <div className="flex flex__gap-sm flex__wrap flex__sp-center">
+        {tour?.photos?.map((el) => (
+          <img className="tour__photo" key={el} src={el} />
+        ))}
+      </div>
     </section>
   );
 };
