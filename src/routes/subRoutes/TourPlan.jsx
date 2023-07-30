7572c21404e30624bf2c528921e84e13201bbf74
JorgeMioska <quispechilojorge@gmail.com>
2023-07-29 22:26:38 -0500

    tour plan styles-1

--- src/routes/subRoutes/TourPlan.jsx
+++ src/routes/subRoutes/TourPlan.jsx
@@ -5,14 +5,19 @@ export const TourPlan = () => {
   const { tour } = useContext(TourContext);
 
   return (
-    <section>
+    <section className="width__80">
       {tour?.tourPlan?.map((visit, index) => (
-        <div key={index}>
+        <div
+          key={index}
+          className="flex flex__column flex__gap-sm pd-md pd-left__md"
+        >
           <h3>{visit[`Day ${index + 1}`]}</h3>
-          <p>{visit.description}</p>
-          <ul>
+          <p className="tour-info__body">{visit.description}</p>
+          <ul className="pd-sm">
             {Object.entries(visit.steps).map(([stepNumber, stepText]) => (
-              <li key={stepNumber}>{stepText}</li>
+              <li className="tour-info__body-sm" key={stepNumber}>
+                {stepText}
+              </li>
             ))}
           </ul>
         </div>
