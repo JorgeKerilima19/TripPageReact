5f35938a8fb7b2174791cdb6bf87065f3cb48274
JorgeMioska <quispechilojorge@gmail.com>
2023-08-02 23:25:08 -0500

    reviwes

--- src/routes/subRoutes/TourReviews.jsx
+++ src/routes/subRoutes/TourReviews.jsx
@@ -1,3 +1,15 @@
 export const TourReviews = () => {
-  return <div>TourReviews</div>;
+  return (
+    <>
+      <h2>Tour Reviews and Scores</h2>
+      <p className="tour-info__body">
+        This is what people have to say about the tour and place, feel free to
+        share your opinion with honesty and help us to improve our service and
+        our connections.
+      </p>
+      <div>
+        <div></div>
+      </div>
+    </>
+  );
 };
