522a92b7b9f3bf9d3b01be6f02c761db11fd7c15
JorgeMioska <quispechilojorge@gmail.com>
2023-08-04 23:27:35 -0500

    react-icons + stars styling

--- src/jsx/OpinionCard.jsx
+++ src/jsx/OpinionCard.jsx
@@ -1,15 +1,27 @@
+import { FaStar } from "react-icons/fa6";
+
 const OpinionCard = ({ el, arr }) => {
   return (
-    <article>
-      <div>Photo</div>
+    <article className="flex flex__gap-md">
+      <img className="tour__user-photo" src={el.photo} alt="userPhoto" />
       <div>
         <h2>{el.userName}</h2>
         <p>{el.comment}</p>
-        <div>
+        <div className="flex flex__wrap flex__sp-center">
           {Object.entries(arr).map(([key, value], index) => (
-            <div key={index}>
-              <strong>{key}:</strong>
-              <span>{value}</span>
+            <div
+              className="flex flex__gap-sm flex__wrap rating__container"
+              key={index}
+            >
+              <strong>{key}</strong>
+              <div className="star__container">
+                {[...Array(5)].map((el, index) => (
+                  <FaStar
+                    key={index}
+                    className={`tour__rating-star ${index < value ? "on" : ""}`}
+                  />
+                ))}
+              </div>
             </div>
           ))}
         </div>
