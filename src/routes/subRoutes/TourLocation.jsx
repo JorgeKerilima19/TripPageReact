08c092a37c5c539d0dc10bc059771bce26f2b009
JorgeMioska <quispechilojorge@gmail.com>
2023-08-05 03:54:37 -0500

    location position changed

--- src/routes/subRoutes/TourLocation.jsx
+++ src/routes/subRoutes/TourLocation.jsx
@@ -1,11 +1,15 @@
 import { useContext } from "react";
-import { TourContext } from "../../context/tourContext";
+import { TourContext } from "../../context/TourContext";
 
 export const TourLocation = () => {
   const { tour } = useContext(TourContext);
   return (
     <>
       <h2 className="section__title">Location</h2>
+      <article className="flex flex__column flex__gap-bg">
+        <header className="section__subtitle">About the Place</header>
+        <p className="tour-info__body pd-bottom__bg">{tour.history}</p>
+      </article>
       <iframe
         src={tour.map}
         className="tour__map"
@@ -14,10 +18,6 @@ export const TourLocation = () => {
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
       ></iframe>
-      <article className="flex flex__column flex__gap-bg">
-        <header className="section__subtitle">About the Place</header>
-        <p className="tour-info__body pd-bottom__bg">{tour.history}</p>
-      </article>
     </>
   );
 };
