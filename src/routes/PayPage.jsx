aa4426cd1c172e6d46b864992a15eb6e1cbc3845
JorgeMioska <quispechilojorge@gmail.com>
2023-08-16 04:18:23 -0500

    paypage itemlist styles

--- src/routes/PayPage.jsx
+++ src/routes/PayPage.jsx
@@ -15,7 +15,7 @@ export const PayPage = () => {
   const { wishList, removeFromCart } = useContext(TourContext);
 
   return (
-    <section className="section__pay flex pd-bg flex__container">
+    <section className="section__pay flex flex__container">
       <div className="flex flex__gap-md flex__item-center">
         <IoIosCart className="pay__svg-cart" />
         <span className="font__1-1">
@@ -35,34 +35,38 @@ export const PayPage = () => {
                 <li key={index} className="flex flex__gap-md flex__item-center">
                   <span className="font__1-2">{index + 1}</span>
                   <div
-                    className="pd-sm cart__item flex flex__sp-btw flex__item-center flex__gap-sm width__full flex__wrap"
+                    className="cart__item-container cart__item width__full "
                     key={el.name}
                   >
-                    <div className="flex flex__item-center flex__gap-md">
+                    <div className="grid__area-1 flex flex__item-center flex__gap-md">
                       <img
                         className="cart__item-img"
                         src={el.banner}
                         alt="element__img"
                       />
-
                       <span className="font__wg-600">{el.name}</span>
                     </div>
-                    <span className="color__secondary font__1-1 font__wg-600">
-                      ${el.pricePerPerson} x <span>{el.tickets}</span>
-                    </span>
-                    <span className="color__tertiary font__1-1 font__wg-600">
-                      {el.tickets * el.pricePerPerson}
-                    </span>
-                    <div></div>
-                    <div className="flex flex__gap-md">
+                    <div className="grid__area-2 color__secondary font__1-1 font__wg-600">
+                      <span>Tickets x person</span>
+                      <div>
+                        <span>${el.pricePerPerson}</span> x{" "}
+                        <span>{el.tickets}</span>
+                      </div>
+                    </div>
+                    <div className="grid__area-3 flex flex__gap-sm flex__item-center button__container">
                       <IoIosTrash
+                        className="cart__button"
                         onClick={() => {
                           removeFromCart(el);
                         }}
                       />
-                      <Link to={el.url}>
-                        <IoIosReturnLeft />
+                      <Link to={el.url} className="flex flex__item-center">
+                        <IoIosReturnLeft className="cart__button" />
                       </Link>
+                    </div>
+                    <div className="grid__area-4 color__tertiary font__1-1 font__wg-600">
+                      <span>Subtotal</span>
+                      <span>${el.tickets * el.pricePerPerson}</span>
                     </div>
                   </div>
                 </li>
