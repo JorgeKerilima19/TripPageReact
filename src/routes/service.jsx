89cf57fa05dec499d40e11d0efcb555276bcdc6e
JorgeMioska <quispechilojorge@gmail.com>
2023-08-21 05:22:52 -0500

    CEO component

--- src/routes/service.jsx
+++ src/routes/service.jsx
@@ -5,13 +5,14 @@ import { Destinations as data } from "../helpers/data/destination";
 import ServicesSlider from "../jsx/ServicesSlider";
 
 import "../styles/service.css";
+import CEO from "../jsx/CEO";
 
 export function Services() {
   return (
     <>
       <HeroImage src={"Service"} />
-      <h2 className="section-title">Countries To visit</h2>
-      <div className="section__container flex__gap-bg pd-bg mg-bottom__bg ">
+      <div className="section__container flex__gap-bg pd-bg">
+        <h2 className="section-title">Countries To visit</h2>
         <section className="homepage__destinations">
           {data.map((item, index) => {
             return <Destinations key={index} item={item} />;
@@ -19,6 +20,8 @@ export function Services() {
         </section>
         <h2 className="section-title">Popular Destinations</h2>
         <ServicesSlider />
+        <h2></h2>
+        <CEO />
       </div>
     </>
   );
